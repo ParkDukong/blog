4a6ec8e469d8d3622de9b127b3edb145c792b117
Dokong2 <jiwan6942@gmail.com>
2025-02-20 09:39:17 +0900

    restudy

---
+++ src/components/ListView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mt-4">
+        <h5>{{ blog.title }}</h5>
+        <p>블로그 글 날짜</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props : {
+            blog : Array
+        }
+    }
+</script>
+
+<style>
+
+</style>
